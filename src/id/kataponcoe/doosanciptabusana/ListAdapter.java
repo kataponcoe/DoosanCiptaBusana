31833575f212eebe11de6a6bc8ea735c8a7934a8
Adriyadi Savana <adrie4mac@gmail.com>
2016-03-25 20:43:16 +0700

    add adapter

--- src/id/kataponcoe/doosanciptabusana/ListAdapter.java
+++ src/id/kataponcoe/doosanciptabusana/ListAdapter.java
@@ -1,14 +1,19 @@
 package id.kataponcoe.doosanciptabusana;
 
+import java.util.List;
+
+import android.R;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class ListAdapter extends BaseAdapter 
 {
+	List<String> menu;
 	
-	public ListAdapter() {
-		
+	public ListAdapter(List<String> listMenu) {
+		this.menu = listMenu;
 	}
 
 	@Override
@@ -32,7 +37,29 @@ public class ListAdapter extends BaseAdapter
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		final ViewHolder holder;
+
+        if (convertView == null)
+        {
+            convertView = View.inflate(parent.getContext(),, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        
+        return convertView;
+	}
+	
+	class ViewHolder
+	{
+		public TextView menuName; 
+		
+		public ViewHolder(View v) {
+			menuName = (TextView) v.findViewById(R.id.row)
+		}
 	}
 
 }
